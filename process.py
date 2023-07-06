8eb7dcf30f7fd5acc39db66c89296c93c1b929cb
NischayYadav615 <112478816+NischayYadav615@users.noreply.github.com>
2023-07-06 19:07:43 +0530

    Update process.py

--- process.py
+++ process.py
@@ -1,7 +1,7 @@
 import os
 import openai
 
-openai.api_key = "XXXXXXXXXXXXXXX" # Replace with your key
+openai.api_key = "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" # Replace with your key
 completion = openai.Completion()
 
 start_sequence = "\nTrigan:"
